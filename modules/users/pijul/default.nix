e4a4fdd386b910e367698456143fc447e515bc36
Jordan Isaacs <mail@jdisaacs.com>
2022-02-08 10:28:29 -0500

    commented out pijul config, waiting for ssh-agent support

--- modules/users/pijul/default.nix
+++ modules/users/pijul/default.nix
@@ -42,35 +42,35 @@ in
       pijul
     ];
 
-    xdg.configFile = {
-      "pijul/publickey.json".source = ./publickey.json;
+    #xdg.configFile = {
+    #  "pijul/publickey.json".source = ./publickey.json;
 
-      "pijul/config.toml".text = ''
-        [author]
-        name = "${cfg.username}"
-        full_name = "${cfg.fullName}"
-        email = "${cfg.email}"
-      '';
-    } // (
-      let
-        public_key = builtins.fromJSON (readFile ./publickey.json);
-      in
-      {
-        "pijul/identities/${public_key.key}".text = builtins.toJSON {
-          inherit public_key;
-          login = "nixos";
-        };
-      }
-    );
+    #  "pijul/config.toml".text = ''
+    #    [author]
+    #    name = "${cfg.username}"
+    #    full_name = "${cfg.fullName}"
+    #    email = "${cfg.email}"
+    #  '';
+    #} // (
+    #  let
+    #    public_key = builtins.fromJSON (readFile ./publickey.json);
+    #  in
+    #  {
+    #    "pijul/identities/${public_key.key}".text = builtins.toJSON {
+    #      inherit public_key;
+    #      login = "nixos";
+    #    };
+    #  }
+    #);
 
-    homeage.file = {
-      "pijulsecret" = {
-        source = cfg.secretKey;
-        decryptPath = "pijul/secretkey.json";
-        lnOnStartup = [
-          "${config.xdg.configHome}/pijul/secretkey.json"
-        ];
-      };
-    };
+    #homeage.file = {
+    #  "pijulsecret" = {
+    #    source = cfg.secretKey;
+    #    decryptPath = "pijul/secretkey.json";
+    #    lnOnStartup = [
+    #      "${config.xdg.configHome}/pijul/secretkey.json"
+    #    ];
+    #  };
+    #};
   };
 }
