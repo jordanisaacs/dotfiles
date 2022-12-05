5be3519b612e8c3346d6074f3921829228c9b761
Jordan Isaacs <mail@jdisaacs.com>
2022-12-04 19:04:33 -0500

    add export_alias function for direnv

--- modules/users/applications/direnv/default.nix
+++ modules/users/applications/direnv/default.nix
@@ -28,6 +28,25 @@ in {
             echo -n "$PWD" | ${pkgs.perl}/bin/shasum | cut -d ' ' -f 1
           )}"
         }
+
+        export IS_DIRENV=1
+
+        export_alias() {
+          local name=$1
+          shift
+          local alias_dir=${config.xdg.cacheHome}/direnv/aliases
+          local target="$alias_dir/$name"
+          local oldpath="$PATH"
+          mkdir -p "$alias_dir"
+          if ! [[ ":$PATH:" == *":$alias_dir:"* ]]; then
+            PATH_add "$alias_dir"
+          fi
+
+          echo "#!/usr/bin/env bash" > "$target"
+          echo "PATH=$oldpath" >> "$target"
+          echo "$@" >> "$target"
+          chmod +x "$target"
+        }
       '';
     };
   };
