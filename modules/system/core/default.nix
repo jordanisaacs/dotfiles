8c5e02d45c2d49891e811543259f3b54845de478
Jordan Isaacs <mail@jdisaacs.com>
2023-03-19 03:02:17 -0400

    add crate2nix cache

--- modules/system/core/default.nix
+++ modules/system/core/default.nix
@@ -80,10 +80,12 @@ in {
         substituters = [
           "https://cache.nixos.org"
           "https://nixpkgs-wayland.cachix.org"
+          "https://eigenvalue.cachix.org"
         ];
         trusted-public-keys = [
           "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
           "nixpkgs-wayland.cachix.org-1:3lwxaILxMRkVhehr5StQprHdEo4IrE8sRho9R9HOLYA="
+          "eigenvalue.cachix.org-1:ykerQDDa55PGxU25CETy9wF6uVDpadGGXYrFNJA3TUs="
         ];
       };
     };
