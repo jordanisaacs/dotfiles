d95aa00f9bfc028200789c1b691c9198d195952f
Jordan Isaacs <jordan@snowytrees.dev>
2023-09-03 20:55:37 -0700

    add kernel development workflow

---
+++ modules/users/kernel/default.nix
@@ -0,0 +1,21 @@
+{
+  pkgs,
+  config,
+  lib,
+  ...
+}:
+with lib; let
+  cfg = config.jd.kernel;
+in {
+  # Based on https://josefbacik.github.io/kernel/2021/10/18/lei-and-b4.html
+  options.jd.kernel.enable = mkEnableOption "kernel development workflow";
+
+  config = mkIf cfg.enable {
+    home.packages = with pkgs; [
+      b4
+      public-inbox # lei
+      msmtp
+      mblaze
+    ];
+  };
+}
